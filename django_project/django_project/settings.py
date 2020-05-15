dc56a65474311fcc7137b04a8b2a0fdb5b91f251
lukas <lukas@waytation.com>
2020-05-15 12:14:35 +0200

    added secrets config example

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import configparser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,9 +136,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'calendar'
 LOGIN_URL = 'login'
 
+DEFAULT_FROM_EMAIL = 'workorbit@gmail.com'
+SERVER_EMAIL = 'workorbit@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER='lukas@waytation.com'
-EMAIL_HOST_PASSWORD='uxgordaiwhyqrozd' 
+
+configParser = configparser.RawConfigParser()   
+configFilePath = './secrets.config'
+configParser.read(configFilePath)
+    
+EMAIL_HOST_USER = configParser.get('secrets_online_booking', 'HOST')
+EMAIL_HOST_PASSWORD = configParser.get('secrets_online_booking', 'PWD')
+
